5dc1d21253b7c7e57b1a620b90308f2eb5759a15
AdamHubYT <125209143+AdamHubYT@users.noreply.github.com>
2026-04-12 17:43:09 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,23 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 import asyncio
 
-bot = Bot("8629214575:AAE8l0FcxWWxEjRjeZvO-lgmaU2I-izlwW0")
+BOT_TOKEN = "8629214575:AAE8l0FcxWWxEjRjeZvO-lgmaU2I-izlwW0"
+
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+WEBAPP_URL = "https://adamhubyt.github.io/bot/?v=5"  # твой GitHub Pages
 
 kb = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="🚀 Играть", web_app=WebAppInfo(
-            url="https://adamhubyt.github.io/bot/?v=3"
-        ))]
+        [KeyboardButton(text="🚀 Играть", web_app=WebAppInfo(url=WEBAPP_URL))]
     ],
     resize_keyboard=True
 )
 
 @dp.message()
 async def start(msg: types.Message):
-    await msg.answer("Жми играть", reply_markup=kb)
+    await msg.answer("Нажми и играй 👇", reply_markup=kb)
 
 async def main():
     await dp.start_polling(bot)
