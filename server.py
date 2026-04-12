31bab228e0857401c4f11263a2bcaa439d41a7ed
AdamHubYT <125209143+AdamHubYT@users.noreply.github.com>
2026-04-12 22:31:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 🔐 ВСТАВЬ СВОИ (лучше через ENV на Render)
+# 🔐 ВСТАВЬ СВОИ (лучше через ENV\ на Render)
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
